8d58275370e3573ec42f16af2ea42fbc6f0e3d82
ethanvannest <ethan3vannest@gmail.com>
2022-12-02 13:29:07 -0700

    created routes for pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,31 @@
+//Components
 import Footer from './components/Footer.js'
 import Header from './components/Header';
 import Projects from './components/Projects';
-import SideMenu from './components/SideMenu';
+
+//Page Routes
+import AboutMe from './PageRoutes/AboutMe.js';
+import Contact from './PageRoutes/Contact.js';
+
+//Dependencies
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Skills from './PageRoutes/Skills.js';
+
 function App() {
   return (
     <div>
-    < Header/>
-    < Projects/>
-    < SideMenu/>
-    < Footer/>
+      <Router>
+      < Header/>
+        <Routes>
+          <div>
+            <Route path ='/' element ={<AboutMe/>}/>
+            <Route path='/projects' element={< Projects/>}/>
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/skills' element={<Skills/>}/>
+          </div>
+            < Footer/>
+        </Routes>
+      </Router>
     </div>
   );
 }
