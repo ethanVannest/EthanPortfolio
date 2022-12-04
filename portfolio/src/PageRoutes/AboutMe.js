0ff15882f9bc4e55887d2b959eec9a6e5e7bf58f
ethanvannest <ethan3vannest@gmail.com>
2022-12-04 15:29:29 -0700

    added about me text

--- portfolio/src/PageRoutes/AboutMe.js
+++ portfolio/src/PageRoutes/AboutMe.js
@@ -4,7 +4,21 @@ const AboutMe = () => {
   return (
     <div className='AboutPage'>
         <img src={require('../images/Ethan_headshot.jpeg')} alt={pictureName}/>
-        
+        <div>
+
+            <p>
+                Hello! My name is Ethan Vannest, I am a frontend developer from Colorado. 
+            </p>
+            <p>
+                I love to create attractice looking products that are user intuitive, that also have something a bit unique to it.
+            </p>
+            <p>
+                I also am a huge fan of music and anything that pertains to video games, reading, and/or cars. 
+            </p>
+            <p>
+                Feel free to reach out with any comments, questions, or concerns. I love to work on ambitous projects with positive and driven people.
+            </p>
+        </div>
     </div>
   )
 }
