788f0e1907624265c819cbd000920a1a861640dc
ethanvannest <ethan3vannest@gmail.com>
2022-12-03 22:36:14 -0700

    added images of projects

--- portfolio/src/PageRoutes/AboutMe.js
+++ portfolio/src/PageRoutes/AboutMe.js
@@ -4,8 +4,8 @@ const AboutMe = () => {
   return (
     <div className='AboutPage'>
         <img src={require('../images/Ethan_headshot.jpeg')} alt={pictureName}/>
+        
     </div>
   )
 }
-// {require(pic1)}
 export default AboutMe
--- portfolio/src/PageRoutes/Projects.js
+++ portfolio/src/PageRoutes/Projects.js
@@ -1,17 +1,33 @@
 import React from 'react'
+import '../App.css'
+
 
 const Projects = () => {
-  let pj1 = '../images/Greenly.png'
   let n1 = 'greenly'
-  let pj2 = '../images/breakout.png'
   let n2 = 'breakout'
-  let pj3 = '../images/reshop.png'
   let n3 = 'reshopper'
-  let pj4 = '../images/tam.png'
   let n4 = 'tamagotchi'
   return (
     <div>
-      Current Projects
+      <h3>Current Projects</h3>
+      <div>
+
+        {/* Greenly */}
+      <img className='greenlyImg' src={require('../images/Greenly.png')} alt={n1}/>
+
+
+        {/* Breakout */}
+      <img className='breakoutImg' src={require('../images/breakout.png')} alt={n2}/>
+
+
+        {/* Reshopper */}
+      <img className='reshopperImg' src={require('../images/reshop.png')} alt={n3}/>
+
+
+        {/* Tamagotchi */}
+      <img className='tamagotchiImg' src={require('../images/tam.png')} alt={n4}/>
+      </div>
+
     </div>
   )
 }
