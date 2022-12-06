3fe99c22d6f7a7bf7050bc2441b6c24ccb3823da
ethanvannest <ethan3vannest@gmail.com>
2022-12-06 14:03:06 -0700

    created about me styling

--- portfolio/src/PageRoutes/AboutMe.js
+++ portfolio/src/PageRoutes/AboutMe.js
@@ -8,22 +8,25 @@ const AboutMe = () => {
     let pictureName = 'headshot'
   return (
     <div className='AboutPage'>
-        <img src={require('../images/Ethan_headshot.jpeg')} alt={pictureName}/>
-        <div>
-
+            <div className='imgDiv'>
+                <img className='headshot' src={require('../images/Ethan_headshot.jpeg')} alt={pictureName}/>
+            </div>
+    <div className='textContainer'>
+        <div className='textDiv'>
             <p>
-                Hello! My name is Ethan Vannest, I am a frontend developer from Colorado. 
+                > Hello! My name is Ethan Vannest, I am a frontend developer from Colorado. 
             </p>
             <p>
-                I love to create attractice looking products that are user intuitive, that also have something a bit unique to it.
+                > I love to create attractice looking products that are user intuitive, that also have something a bit unique to it.
             </p>
             <p>
-                I also am a huge fan of music and anything that pertains to video games, reading, and/or cars. 
+                > I also am a huge fan of music and anything that pertains to video games, reading, and/or cars. 
             </p>
             <p>
-                Feel free to reach out with any comments, questions, or concerns. I love to work on ambitous projects with positive and driven people.
+                > Feel free to reach out with any comments, questions, or concerns. I love to work on ambitous projects with positive and driven people.
             </p>
         </div>
+    </div>
     </div>
   )
 }
